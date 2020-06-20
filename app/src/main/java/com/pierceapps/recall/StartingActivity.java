6fab40c5ba982d4f6db74bbf1f81c2b40387b68b
Nelsorek <37481582+Nelsorek@users.noreply.github.com>
2020-06-20 14:52:25 -0400

    Initial commit: Main page and Activity page initialized

---
+++ app/src/main/java/com/pierceapps/recall/StartingActivity.java
@@ -0,0 +1,14 @@
+package com.pierceapps.recall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
